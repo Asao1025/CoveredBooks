71d7269e7ce711a6991cdfcb115cef8f3b25058d
Asao1025 <asx2858@gmail.com>
2024-11-08 00:43:29 +0900

    カテゴリーボタンの元を実装

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -1,12 +1,14 @@
 import TopImageComponent from './topImage';
 import FirstMessage from './firstMessage';
 import PopContents from './popContents';
+import CategoryButtons from './categoryButtons'
 
 export default function page() {
   return (
     <body>
       <TopImageComponent />
       <FirstMessage />
+      <CategoryButtons />
       <PopContents />
       <a href="../loading">アンカーテキスト</a>
 
