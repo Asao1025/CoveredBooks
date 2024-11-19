081bd93acdd4f0ee8e6f5376a880f36c1613fb1e
Asao1025 <asx2858@gmail.com>
2024-11-19 23:42:10 +0900

    モーダルの閉じるボタンを実装

--- src/app/pages/modal.tsx
+++ src/app/pages/modal.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { CloseOutlined } from '@ant-design/icons';
+import { FloatButton} from 'antd';
 
 const Modal = (Props) =>　{
     const closeModal = () => {
@@ -9,13 +11,21 @@ const Modal = (Props) =>　{
     return (
         <div className="modalBack">
             <div className="modalContent">
-                <div className="introductionImg">
+
+                <div className="ModalImg">
                     <img
                     src="./image/pops/testPop1.jpg"
-                    onClick={closeModal}
                     alt="Sample Imageだよ" />
+                    <FloatButton className="closeButton"
+                        icon={<CloseOutlined />}
+                        onClick={closeModal}
+                        style={{
+                           position: 'absolute',
+                           top: -10,
+                           right: -10 ,
+                           zIndex: 10}}/>
                 </div>
-                <div className="introductionText">
+                <div className="modalText">
                     ここに説明文を記載する<br/>
                 </div>
             </div>
